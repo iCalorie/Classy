852211db46a1975b3afc956d76a49c8dca4dc93e
Peyman Khanjan <p.khanjan@gmail.com>
2017-05-26 15:38:50 +0100

    Conform CASUnitToken to NSCoding.

--- Classy/Parser/CASUnitToken.m
+++ Classy/Parser/CASUnitToken.m
@@ -12,6 +12,23 @@
 
 #pragma mark - Helpers
 
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        self.suffix = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(suffix))];
+        self.rawValue = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(rawValue))];
+    }
+    
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder
+{
+    [super encodeWithCoder:coder];
+    
+    [coder encodeObject:self.suffix forKey:NSStringFromSelector(@selector(suffix))];
+    [coder encodeObject:self.rawValue forKey:NSStringFromSelector(@selector(rawValue))];
+}
+
 - (NSString *)stringValue {
     return self.rawValue;
 }
