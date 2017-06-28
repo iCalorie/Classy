1a9a8c5c6c54454fc8c649dfa21dcd833b23f021
ChachiQ <qiuqi830@gmail.com>
2017-06-28 16:23:14 +0800

    optimize UIFont style property

--- Classy/Parser/CASStyleProperty.m
+++ Classy/Parser/CASStyleProperty.m
@@ -14,11 +14,11 @@
 
 @interface CASStyleProperty ()
 
-@property (nonatomic, strong, readwrite) NSString *name;
-@property (nonatomic, strong, readwrite) NSArray *values;
+@property(nonatomic, strong, readwrite) NSString *name;
+@property(nonatomic, strong, readwrite) NSArray *values;
 
-@property (nonatomic, strong, readwrite) CASToken *nameToken;
-@property (nonatomic, strong, readwrite) NSArray *valueTokens;
+@property(nonatomic, strong, readwrite) CASToken *nameToken;
+@property(nonatomic, strong, readwrite) NSArray *valueTokens;
 @end
 
 @implementation CASStyleProperty {
@@ -165,68 +165,69 @@
         *color = colorValue;
         return YES;
     }
-    
+
     NSString *value = [self valueOfTokenType:CASTokenTypeRef]
-    ?: [self valueOfTokenType:CASTokenTypeSelector]
-    ?: [self valueOfTokenType:CASTokenTypeString];
-    
-    
+            ?: [self valueOfTokenType:CASTokenTypeSelector]
+                    ?: [self valueOfTokenType:CASTokenTypeString];
+
+
     CASToken *alphaToken = [self.valueTokens lastObject];
     CGFloat alphaTokenValue = 1.0;
-    
+
     BOOL hasAlphaToken = NO;
-    
+
     if (alphaToken.type == CASTokenTypeUnit && alphaToken.value != nil) {
         float floatValue = [alphaToken.value floatValue];
-        
+
         if (floatValue < 1 && floatValue >= 0) {
             hasAlphaToken = YES;
             alphaTokenValue = floatValue;
         }
     }
-    
+
     if ([value isEqualToString:@"rgb"] || [value isEqualToString:@"rgba"] || [value isEqualToString:@"hsl"] || [value isEqualToString:@"hsla"]) {
         NSArray *unitTokens = [self consecutiveValuesOfTokenType:CASTokenTypeUnit];
-        
+
         CGFloat alpha = 1.0;
-        
+
         // invalid if you don't have 3 colors
-        if(unitTokens.count < 3) {
+        if (unitTokens.count < 3) {
             return NO;
-        } else if(unitTokens.count == 4){
+        } else if (unitTokens.count == 4) {
             alpha = [unitTokens[3] doubleValue];
         }
-        
+
         if (hasAlphaToken) {
             alpha = alphaTokenValue;
         }
-        
-        if( [value isEqualToString:@"rgb"] || [value isEqualToString:@"rgba"] ) {
-            *color = [UIColor colorWithRed:[unitTokens[0] doubleValue]/255.0 green:[unitTokens[1] doubleValue]/255.0 blue:[unitTokens[2] doubleValue]/255.0 alpha:alpha];
-        } else if ( [value isEqualToString:@"hsl"] || [value isEqualToString:@"hsla"] ) {
-            *color = [UIColor colorWithHue:[unitTokens[0] doubleValue]/360.0 saturation:[unitTokens[1] doubleValue]/100.0 brightness:[unitTokens[2] doubleValue]/100.0 alpha:alpha];
+
+        if ([value isEqualToString:@"rgb"] || [value isEqualToString:@"rgba"]) {
+            *color = [UIColor colorWithRed:[unitTokens[0] doubleValue] / 255.0 green:[unitTokens[1] doubleValue] / 255.0 blue:[unitTokens[2] doubleValue] / 255.0 alpha:alpha];
+        } else if ([value isEqualToString:@"hsl"] || [value isEqualToString:@"hsla"]) {
+            *color = [UIColor colorWithHue:[unitTokens[0] doubleValue] / 360.0 saturation:[unitTokens[1] doubleValue] / 100.0 brightness:[unitTokens[2] doubleValue] / 100.0 alpha:alpha];
         }
         return YES;
     }
-    
+
     value = [value cas_stringByCamelCasing];
-    
+
     SEL selector = NSSelectorFromString([NSString stringWithFormat:@"%@Color", value]);
     if (selector && [UIColor.class respondsToSelector:selector]) {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-        
+
         if (hasAlphaToken) {
             *color = [[UIColor.class performSelector:selector] colorWithAlphaComponent:alphaTokenValue];
         } else {
             *color = [UIColor.class performSelector:selector];
         }
-        
+
 #pragma clang diagnostic pop
         return YES;
     }
-    
-    return NO;}
+
+    return NO;
+}
 
 - (BOOL)transformValuesToNSString:(NSString **)string {
     NSString *value = [self valueOfTokenType:CASTokenTypeString]
@@ -243,29 +244,29 @@
 - (BOOL)transformValuesToUIImage:(UIImage **)image {
     UIEdgeInsets insets;
     UIColor *tintColor = nil;
-    
+
     NSString *imageName = [self valueOfTokenType:CASTokenTypeString] ?: [self valueOfTokenType:CASTokenTypeRef];
-    
+
     BOOL hasInsets = [self transformValuesToUIEdgeInsets:&insets];
-    
+
     BOOL hasTintColor = NO;
-    
+
     if (self.valueTokens.count > 1) {
         // HACK to get the tintColor out.
         NSMutableArray *colorTokens = [NSMutableArray arrayWithArray:self.valueTokens];
         [colorTokens removeObjectAtIndex:0]; // Remove the image
-        
+
         CASStyleProperty *styleProperty = [[CASStyleProperty alloc] initWithNameToken:self.nameToken valueTokens:colorTokens];
-        
+
         [styleProperty transformValuesToUIColor:&tintColor];
-        
+
         hasTintColor = tintColor != nil;
         // HACK
     }
-    
+
     UIImage *imageValue = nil;
     NSRange schemeRange = [imageName rangeOfString:@"://"];
-    if(schemeRange.location != NSNotFound) {
+    if (schemeRange.location != NSNotFound) {
 
         // We are a file path instead
         NSString *scheme = [imageName substringToIndex:schemeRange.location];
@@ -274,15 +275,15 @@
         // Checking if we're fetching from one of our built in
         // document uris
         NSSearchPathDirectory searchMask = 0;
-        if([scheme isEqualToString:@"caches"]) {
+        if ([scheme isEqualToString:@"caches"]) {
             searchMask = NSCachesDirectory;
-        } else if([scheme isEqualToString:@"documents"]) {
+        } else if ([scheme isEqualToString:@"documents"]) {
             searchMask = NSDocumentDirectory;
-        } else if([scheme isEqualToString:@"appsupport"]) {
+        } else if ([scheme isEqualToString:@"appsupport"]) {
             searchMask = NSApplicationSupportDirectory;
         }
 
-        if(searchMask != 0) {
+        if (searchMask != 0) {
             // If we found a search mask, then use that
             NSArray *paths = NSSearchPathForDirectoriesInDomains(searchMask, NSUserDomainMask, YES);
             NSString *imagePath = [paths firstObject];
@@ -300,7 +301,7 @@
     if (hasTintColor && tintColor) {
         imageValue = [imageValue tintedWithColor:tintColor];
     }
-    
+
     if (hasInsets) {
         imageValue = [imageValue resizableImageWithCapInsets:insets];
     }
@@ -311,12 +312,14 @@
     return NO;
 }
 
-- (BOOL)transformValuesToUIFont:(UIFont **)font
-{
-    NSNumber * fontSize = [self valueOfTokenType:CASTokenTypeUnit];
-    NSString * fontName = [self valueOfTokenType:CASTokenTypeString]
+- (BOOL)transformValuesToUIFont:(UIFont **)font {
+    NSNumber *fontSize = [self valueOfTokenType:CASTokenTypeUnit];
+    NSString *fontName = [self valueOfTokenType:CASTokenTypeString]
             ?: [self valueOfTokenType:CASTokenTypeRef]
                     ?: [self valueOfTokenType:CASTokenTypeSelector];
+    if (fontName) {
+        fontName = [fontName lowercaseString];
+    }
 
     if (!fontSize && !fontName.length) {
         return NO;
@@ -325,51 +328,51 @@
     static NSDictionary *textStyleLookupMap = nil;
     if (!textStyleLookupMap) {
         textStyleLookupMap = @{
-               @"title1" : UIFontTextStyleTitle1,
-               @"title2" : UIFontTextStyleTitle2,
-               @"title3" : UIFontTextStyleTitle3,
-               @"headline" : UIFontTextStyleHeadline,
-               @"body" : UIFontTextStyleBody,
-               @"callout" : UIFontTextStyleCallout,
-               @"subhead" : UIFontTextStyleSubheadline,
-               @"footnote" : UIFontTextStyleFootnote,
-               @"caption1" : UIFontTextStyleCaption1,
-               @"caption2" : UIFontTextStyleCaption2,
+                @"title1": UIFontTextStyleTitle1,
+                @"title2": UIFontTextStyleTitle2,
+                @"title3": UIFontTextStyleTitle3,
+                @"headline": UIFontTextStyleHeadline,
+                @"body": UIFontTextStyleBody,
+                @"callout": UIFontTextStyleCallout,
+                @"subhead": UIFontTextStyleSubheadline,
+                @"footnote": UIFontTextStyleFootnote,
+                @"caption1": UIFontTextStyleCaption1,
+                @"caption2": UIFontTextStyleCaption2,
         };
     }
 
     static NSDictionary *textWeightLookupMap = nil;
     if (!textWeightLookupMap) {
         textWeightLookupMap = @{
-                @"ultralight" : @(UIFontWeightUltraLight),
-                @"thin" : @(UIFontWeightThin),
-                @"light" : @(UIFontWeightLight),
-                @"regular" : @(UIFontWeightRegular),
-                @"semibold" : @(UIFontWeightSemibold),
-                @"bold" : @(UIFontWeightBold),
-                @"heavy" : @(UIFontWeightHeavy),
-                @"black" : @(UIFontWeightBlack)
+                @"ultralight": @(UIFontWeightUltraLight),
+                @"thin": @(UIFontWeightThin),
+                @"light": @(UIFontWeightLight),
+                @"regular": @(UIFontWeightRegular),
+                @"semibold": @(UIFontWeightSemibold),
+                @"bold": @(UIFontWeightBold),
+                @"heavy": @(UIFontWeightHeavy),
+                @"black": @(UIFontWeightBlack)
         };
     }
 
-    NSString *textStyle = textStyleLookupMap[fontName];
-    NSNumber *weightValue = textWeightLookupMap[fontName];
+    if (fontName.length) {
+        NSString *textStyle = textStyleLookupMap[fontName];
+        NSNumber *weightValue = textWeightLookupMap[fontName];
 
-    if (textStyle && !fontSize) {
-        *font = [UIFont preferredFontForTextStyle:textStyle];
-    }
-    if (weightValue && fontSize) {
-        CGFloat fontSizeValue = [fontSize floatValue] ?: [UIFont systemFontSize];
-        CGFloat weight = [weightValue floatValue];
-        *font =  [UIFont systemFontOfSize:fontSizeValue weight:weight];
+        if (textStyle) {
+            *font = [UIFont preferredFontForTextStyle:textStyle];
+        } else {
+            CGFloat fontSizeValue = [fontSize floatValue] ?: [UIFont systemFontSize];
+            CGFloat weight = [weightValue floatValue]
+                    ?: [fontSize floatValue]
+                            ?: [UIFont systemFontSize];
+            *font = [UIFont systemFontOfSize:fontSizeValue weight:weight];
+        }
     } else {
         CGFloat fontSizeValue = [fontSize floatValue] ?: [UIFont systemFontSize];
-        if (fontName) {
-            *font = [UIFont fontWithName:fontName size:fontSizeValue];
-        } else {
-            *font = [UIFont systemFontOfSize:fontSizeValue];
-        }
+        *font = [UIFont systemFontOfSize:fontSizeValue];
     }
+
     return YES;
 }
 
